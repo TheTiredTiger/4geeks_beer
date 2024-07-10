3a341df9f80a708cf0f3dbbeac1e4d6491c0537d
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-07-10 19:39:14 +0000

    did all GET requests

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.get("/", (request, response) => {
   response.send("this is my first route");
 });
 
+app.get("/beers", (req, res) => {
+  res.send(beers)
+})
+
+app.get("/beers/:id", (req, res) => {
+  let id = req.params.id;
+  res.send(beers.filter((beer) => {
+    return beer.id === parseInt(id)
+  }))
+  
+})
+
+app.get("/beer/:name", (req, res) => {
+  let name = req.params.name;
+  console.log(name)
+  let something = beers.filter((beer) => {
+    return beer.name === name
+  })
+  res.send(JSON.stringify(something))
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`user connected on port: ${PORT}`);
 });
